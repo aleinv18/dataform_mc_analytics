dee6d17b40743ac27b9f5e3526c683a1e4136173
Alessandro Inversi <inversi.a@farmae.it>
2023-04-28 08:04:13 +0000

    init

---
+++ includes/config.js
@@ -0,0 +1,16 @@
+//Farmaeit
+const FARMAEIT_MC_ID = "109971045"; 
+const FARMAEIT_GCP_ID = "farmaeit-bq-ga4"
+
+//AF
+const AMICAFARMACIA_MC_ID = "6800466";
+const AMICAFARMACIA_GCP_ID = "amicafarmacia-bq-ga4";
+
+module.exports = { 
+    FARMAEIT_MC_ID,
+    FARMAEIT_GCP_ID,
+
+    AMICAFARMACIA_MC_ID,
+    AMICAFARMACIA_GCP_ID
+
+    };
